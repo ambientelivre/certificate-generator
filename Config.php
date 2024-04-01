03f9d7833ea6450208825c16658c93b6c9efaf0e
Miguel Domiciano Vieira <97977665+Migueldv06@users.noreply.github.com>
2024-04-01 16:54:32 -0300

    Create Config.php

---
+++ Config.php
@@ -0,0 +1,14 @@
+<?php 
+$emailhost = "mail.seuDominio.com.br";//coloque seu dominio de email
+$email = "mconta@SeuDomínio.com.br";//coloque o email de saida
+$emailsenha = "senha";//coloque a senha do seu email
+$arquivoCSV = "Nome.csv";//coloque o seu arquivo com as informações csv
+$SeuNome = "Ambiente Livre";//Seu Nome *opcional
+
+
+
+//info do treinamento
+$data = "25/03/2024 a 28/05/2024";
+$horas = "52";
+$curso = "Alfresco";
+?>
