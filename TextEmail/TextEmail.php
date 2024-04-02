84558196c724aacb995943128ff944a20834cc5c
Migueldv06 <brasilgameplay06@gmail.com>
2024-04-02 14:34:55 -0300

    2.0

--- TextEmail/TextEmail.php
+++ TextEmail/TextEmail.php
@@ -1,121 +1,37 @@
 <?php
-
-$mensagem = '
-<!DOCTYPE html>
+$assinatura = '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
 <html>
-  <head>
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+	<title></title>
+	<meta name="generator" content="LibreOffice 6.0.7.3 (Linux)"/>
+	<meta name="created" content="2021-12-27T17:15:21.517347563"/>
+	<meta name="changed" content="2021-12-27T17:18:23.058600299"/>
+	<style type="text/css">
+		@page { margin: 2cm }
+		p { margin-bottom: 0.25cm; line-height: 115% }
+		a:link { so-language: zxx }
+	</style>
+</head>
+<body lang="pt-BR" dir="ltr">
+<p style="margin-bottom: 0cm; line-height: 100%"><font color="#1f497d"><font size="2" style="font-size: 9pt"><b>Miguel Domiciano Vieira – EAD e CRM </b></font></font><font color="#1f497d"><font size="2" style="font-size: 9pt"><b>
+</b></font></font><br/>
+<font color="#1f497d"><font size="2" style="font-size: 9pt"><b>Ambiente
+Livre – </b></font></font><font color="#1f497d"><font size="2" style="font-size: 9pt"><b>Open
+Source for Business</b></font></font><br/>
+<a href="https://www.ambientelivre.com.br/">https://www.ambientelivre.com.br/</a><br/>
+Telefone:
++55 41 3308-3438<br/>
+WhatsApp: <a href="https://api.whatsapp.com/send?phone=5541991210655">+55 41 99121-0655</a><br/>
+Twitter: <a href="https://twitter.com/ambientelivre">@ambientelivre</a><br/>
+Instagram: <a href="https://www.instagram.com/ambientelivreopensoftware/">@ambientelivreopensoftware</a><br/>
+Facebook:
+<a href="https://www.facebook.com/AmbienteLivre/">@ambientelivre</a></p>
+LinkedIn:
+<a href="https://www.linkedin.com/company/ambientelivre/">@ambientelivre</a>
+</body>
+</html>';
 
-    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
-  </head>
-  <body>
-    <div class="moz-text-html" lang="x-unicode"> <br>
-      <div class="moz-text-html" lang="x-unicode">
-        <div class="moz-text-html" lang="x-unicode">
-          <div class="moz-text-html" lang="x-unicode">
-            <div class="moz-text-html" lang="x-unicode">
-              <div class="moz-text-html" lang="x-unicode">
-                <div class="moz-text-html" lang="x-unicode">
-                  <div class="moz-text-html" lang="x-unicode">
-                    <div class="moz-text-html" lang="x-unicode">
-                      <div class="moz-text-html" lang="x-unicode">
-                        <div class="moz-text-html" lang="x-unicode">
-                          <div class="moz-text-html" lang="x-unicode">
-                            <div class="moz-text-html" lang="x-unicode">
-                              <div class="moz-text-html"
-                                lang="x-unicode">
-                                <div class="moz-text-html"
-                                  lang="x-unicode">
-                                  <div class="moz-text-html"
-                                    lang="x-unicode">
-                                    <div class="moz-text-html"
-                                      lang="x-unicode">Boa Tarde
-                                      *\$nome*\,
-                                      <div class="moz-text-html"
-                                        lang="x-unicode">
-                                        <div class="moz-text-html"
-                                          lang="x-unicode">
-                                          <div class="moz-text-html"
-                                            lang="x-unicode">
-                                            <div class="moz-text-html"
-                                              lang="x-unicode">
-                                              <div class="moz-text-html"
-                                                lang="x-unicode">
-                                                <p
-style="line-height: 100%;
-                                margin-bottom: 0cm">O seu certificado de
-                                                  conclusÃ£o do
-                                                  treinamento Kubernetes
-                                                  - 32 horas foi emitido
-                                                  com sucesso, e segue
-                                                  em anexo a este
-                                                  e-mail. </p>
-                                                <p
-style="line-height: 100%;
-                                margin-bottom: 0cm">Desejamos sucesso
-                                                  com a ferramenta. </p>
-                                                <p
-style="line-height: 100%;
-                                margin-bottom: 0cm">Atenciosamente <br>
-                                                </p>
-                                                <p
-style="line-height: 100%;
-                                margin-bottom: 0cm"><br>
-                                                </p>
-                                                <div
-                                                  class="moz-signature">--
-                                                  <br>
-                                                  <p
-style="margin-bottom: 0cm;
-                                  line-height: 100%"><font
-                                                      color="#1f497d"><font
-style="font-size: 9pt" size="2"><b>Miguel Domiciano Vieira â€“ EAD e CRM </b></font></font><font
-                                                      color="#1f497d"><font
-style="font-size: 9pt" size="2"><b> </b></font></font><br>
-                                                    <font
-                                                      color="#1f497d"><font
-style="font-size: 9pt" size="2"><b>Ambiente Livre â€“ </b></font></font><font
-                                                      color="#1f497d"><font
-style="font-size: 9pt" size="2"><b>Open Source for Business</b></font></font><br>
-                                                    <a
-href="https://www.ambientelivre.com.br/" class="moz-txt-link-freetext">https://www.ambientelivre.com.br/</a><br>
-                                                    Telefone: +55 41
-                                                    3308-3438<br>
-                                                    WhatsApp: <a
-href="https://api.whatsapp.com/send?phone=5541991210655">+55 41
-                                                      99121-0655</a><br>
-                                                    Twitter: <a
-href="https://twitter.com/ambientelivre">@ambientelivre</a><br>
-                                                    Instagram: <a
-href="https://www.instagram.com/ambientelivreopensoftware/">@ambientelivreopensoftware</a><br>
-                                                    Facebook: <a
-href="https://www.facebook.com/AmbienteLivre/">@ambientelivre</a></p>
-                                                  LinkedIn: <a
-href="https://www.linkedin.com/company/ambientelivre/">@ambientelivre</a>
-                                                </div>
-                                              </div>
-                                            </div>
-                                          </div>
-                                        </div>
-                                      </div>
-                                    </div>
-                                  </div>
-                                </div>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </body>
-</html>
-';
+$mensagem = "Boa Tarde <strong>$nome</strong>,<br>O seu certificado de conclusão do Treinamento $curso - $horas horas foi emitido com sucesso, e segue em anexo a este e-mail.<br>Desejamos sucesso com a ferramenta.<br>Atenciosamente<br>$assinatura";
 
 ?>
